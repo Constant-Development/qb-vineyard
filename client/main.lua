d169ca7e8d84fe785b157aa2f45bbf6486eff301
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2022-04-28 02:28:26 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,101 +1,119 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Functions
-
-function LoadAnim(dict)
-    while not HasAnimDictLoaded(dict) do
-        RequestAnimDict(dict)
-        Wait(1)
-    end
-end
-
-function PrepareAnim()
-    local ped = PlayerPedId()
-    LoadAnim('amb@code_human_wander_rain@male_a@base')
-    TaskPlayAnim(ped, 'amb@code_human_wander_rain@male_a@base', 'static', 6.0, -6.0, -1, 47, 0, 0, 0, 0)
-end
-
-function PickAnim()
-    local ped = PlayerPedId()
-    LoadAnim('amb@prop_human_bum_bin@idle_a')
-    TaskPlayAnim(ped, 'amb@prop_human_bum_bin@idle_a', 'idle_a', 6.0, -6.0, -1, 47, 0, 0, 0, 0)
+local function Notify(notifType, message, title)
+	if Config.Notify == 'qb' then
+		if notifType == 1 then
+			QBCore.Functions.Notify(message, 'success')
+		elseif notifType == 2 then
+			QBCore.Functions.Notify(message, 'primary')
+		elseif notifType == 3 then
+			QBCore.Functions.Notify(message, 'error')
+		end
+	elseif Config.Notify == "okok" then
+		if notifType == 1 then
+			exports['okokNotify']:Alert(title, message, 3000, 'success')
+		elseif notifType == 2 then
+			exports['okokNotify']:Alert(title, message, 3000, 'info')
+		elseif notifType == 3 then
+			exports['okokNotify']:Alert(title, message, 3000, 'error')
+		end
+	elseif Config.Notify == "mythic" then
+		if notifType == 1 then
+			exports['mythic_notify']:DoHudText('success', message)
+		elseif notifType == 2 then
+			exports['mythic_notify']:DoHudText('inform', message)
+		elseif notifType == 3 then
+			exports['mythic_notify']:DoHudText('error', message)
+		end
+	elseif Config.Notify == 'chat' then
+		TriggerEvent('chatMessage', message)
+	end
 end
 
 function pickProcess()
-    PickAnim()
     QBCore.Functions.Progressbar("pick_grape", "Picking Grapes ..", math.random(6000,8000), false, true, {
         disableMovement = true,
         disableCarMovement = true,
         disableMouse = false,
         disableCombat = true,
+    }, {
+        animDict = "amb@prop_human_bum_bin@idle_a",
+        anim = "idle_a",
+        flags = 6,
     }, {}, {}, {}, function() -- Done
-        ClearPedTasks(PlayerPedId())
+        StopAnimTask(PlayerPedId(), "amb@prop_human_bum_bin@idle_a", "idle_a", 6.0)
 		TriggerServerEvent("qb-vineyard:server:getGrapes")
     end, function() -- Cancel
-        ClearPedTasks(PlayerPedId())
-        QBCore.Functions.Notify("You have cancelled the task", "error")
+        Notify(3, Config.Notifications["TaskCancel"], Config.Notifications["okok_VineyardTitle"])
+        StopAnimTask(PlayerPedId(), "amb@prop_human_bum_bin@idle_a", "idle_a", 6.0)
     end)
 end
 
 function grapeJuiceProcess()
-    PrepareAnim()
     QBCore.Functions.Progressbar("grape_juice", "Processing Grapes ..", math.random(15000,20000), false, true, {
         disableMovement = true,
         disableCarMovement = true,
         disableMouse = false,
         disableCombat = true,
+    }, {
+        animDict = "amb@code_human_wander_rain@male_a@base",
+        anim = "static",
+        flags = 6,
     }, {}, {}, {}, function() -- Done
-        ClearPedTasks(PlayerPedId())
+        StopAnimTask(PlayerPedId(), "amb@code_human_wander_rain@male_a@base", "static", 6.0)
         TriggerServerEvent("qb-vineyard:server:grapeJuice")
     end, function() -- Cancel
-        ClearPedTasks(PlayerPedId())
-        QBCore.Functions.Notify("You have cancelled the task", "error")
+        Notify(3, Config.Notifications["TaskCancel"], Config.Notifications["okok_VineyardTitle"])
+        StopAnimTask(PlayerPedId(), "amb@code_human_wander_rain@male_a@base", "static", 6.0)
     end)
 end
 
 function WineProcess()
-    PrepareAnim()
     QBCore.Functions.Progressbar("grape_juice", "Stirring Wine..", math.random(15000,20000), false, true, {
         disableMovement = true,
         disableCarMovement = true,
         disableMouse = false,
         disableCombat = true,
+    }, {
+        animDict = "amb@code_human_wander_rain@male_a@base",
+        anim = "static",
+        flags = 6,
     }, {}, {}, {}, function() -- Done
-        ClearPedTasks(PlayerPedId())
+        StopAnimTask(PlayerPedId(), "amb@code_human_wander_rain@male_a@base", "static", 6.0)
         TriggerServerEvent("qb-vineyard:server:loadIngredients")
     end, function() -- Cancel
-        ClearPedTasks(PlayerPedId())
-        QBCore.Functions.Notify("You have cancelled the task", "error")
+        Notify(3, Config.Notifications["TaskCancel"], Config.Notifications["okok_VineyardTitle"])
+        StopAnimTask(PlayerPedId(), "amb@code_human_wander_rain@male_a@base", "static", 6.0)
     end)
 end
-
--- Events
 
 RegisterNetEvent('qb-vineyard:pickgrapes')
 AddEventHandler('qb-vineyard:pickgrapes', function()
     pickProcess()
 	Wait(5000)
-	QBCore.Functions.Notify('Begin heading to the back two Doors of the Mansion...', 'primary', 4000)
+    Notify(2, Config.Notifications["PickGrapesNextStepMessage"], Config.Notifications["okok_VineyardTitle"])
 end)
 
-RegisterNetEvent('qb-vineyard:GrapeJuiceProcess')
-AddEventHandler('qb-vineyard:GrapeJuiceProcess', function()
-	grapeJuiceProcess()
+RegisterNetEvent('qb-vineyard:client:ProcessStage')
+AddEventHandler('qb-vineyard:ProcessStage', function()
+    QBCore.Functions.TriggerCallback("QBCore:HasItem", function(HasItem) 
+        if HasItem then
+            grapeJuiceProcess()
+        else
+            WineProcess()
+        end
+    end, Config.ProcessingItem)
 end)
 
-RegisterNetEvent('qb-vineyard:WineProcess')
-AddEventHandler('qb-vineyard:WineProcess', function()
-	WineProcess()
-end)
-
-RegisterNetEvent('qb-vineyard:SellGrapeJuiceMenu')
+RegisterNetEvent('qb-vineyard:client:SellMenu')
 AddEventHandler('qb-vineyard:SellGrapeJuiceMenu', function()
-	TriggerServerEvent("qb-vineyard:server:SellGrapeJuice")
-end)
-
-RegisterNetEvent('qb-vineyard:SellWineMenu')
-AddEventHandler('qb-vineyard:SellWineMenu', function()
-	TriggerServerEvent("qb-vineyard:server:SellWine")
+    QBCore.Functions.TriggerCallback("QBCore:HasItem", function(HasItem) 
+        if HasItem then
+            TriggerServerEvent("qb-vineyard:server:SellGrapeJuice")
+        else
+            TriggerServerEvent("qb-vineyard:server:SellWine")
+        end
+    end, Config.SellItem)
 end)
 
 RegisterNetEvent('qb-vineyard:client:loadIngredients') -- Loads Portion of GrapeJuice into MoonShine
