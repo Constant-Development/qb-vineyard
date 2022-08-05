eb8135e7ee411445df7beb00d74a8e0d2884ec94
Fast-Lane RolePlay <timothyphilipp14@gmail.com>
2022-08-05 10:50:20 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,11 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'SixGrams#0544 | Constant Development'
-description 'QBCore Juice System'
-version '1.0.2'
+description 'QBCore Vineyard Job System Rework by Constant Development'
+version '1.1'
 
 shared_script 'config.lua'
-
-client_scripts {
-    'client/main.lua',
-    'client/menuevents.lua'
-}
-
+client_script 'client/*.lua'
 server_script 'server/main.lua'
 
 lua54 'yes'
